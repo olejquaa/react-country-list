fa85cf572e451169931eb5a9a053571f4b98d4b8
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2022-11-18 09:28:08 +0300

    country-item-added-classNames

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,7 +2,7 @@ import { BadgeLabel } from '../../types'
 
 export const Badge = ({ title, count, color }: BadgeLabel) => {
     return (
-        <span className={`d-flex justify-content-around badge bg-${color} p-2 gap-3`}>
+        <span className={`country-${title}--badge d-flex justify-content-around badge bg-${color} p-2 gap-3`}>
             {title}: {count}
         </span>
     )
--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -12,17 +12,17 @@ export const CountryItem = ({ country }: IProps) => {
                 country.map(({ flags, name, capital, region, area, population }) => {
                     return (
                         <li key={name} className="list-group-item list-group-item-action">
-                            <div className='row align-items-center text-start'>
-                                <span className='col col-lg-1'>
-                                    <img src={flags} className="img-fluid" alt={name} />
+                            <div className='list-group-item--wrapper row align-items-center text-start'>
+                                <span className='list-group-item--country-flag col col-lg-1'>
+                                    <img src={flags} className="country-flag--image img-fluid" alt={name} />
                                 </span>
-                                <span className='col col-lg'>{name}</span>
-                                <span className='col col-lg'>{capital}</span>
-                                <span className='col col-lg'>{region}</span>
-                                <span className='col col-lg'>
+                                <span className='list-group-item--country-name col col-lg'>{name}</span>
+                                <span className='list-group-item--country-capital col col-lg'>{capital}</span>
+                                <span className='list-group-item--country-region col col-lg'>{region}</span>
+                                <span className='list-group-item--country-area col col-lg'>
                                     <Badge title='area' count={area} color={Color.Warning} />
                                 </span>
-                                <span className='col col-lg'>
+                                <span className='list-group-item--country-population col col-lg'>
                                     <Badge title='population' count={population} color={Color.Success} />
                                 </span>
                             </div>
