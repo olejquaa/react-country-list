09c8f1436bf003472015b5d7f8d295f2a72f001f
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2022-11-18 09:45:23 +0300

    hotfix-countryList-added-className

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -8,7 +8,7 @@ interface IProps {
 export const CountryList = ({ country }: IProps) => {
     return (
         <div className="country-list">
-            <h1>Country List </h1>
+            <h1 className="country-list--title">Country List </h1>
             <CountryItem country={country} />
         </div>
     )
